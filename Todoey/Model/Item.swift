3d4cdd8c37f484ec36d38afe261bf662e1635216
greg300 <greg300@comcast.net>
2019-01-17 20:52:07 -0500

    Convert to MVC design pattern.

---
+++ Todoey/Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Gregory Giovannini on 1/17/19.
+//  Copyright © 2019 Gregory Giovannini. All rights reserved.
+//
+
+import Foundation
+
+class Item
+{
+    var title : String = ""
+    var done : Bool = false
+}
